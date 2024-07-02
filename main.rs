541b8b7b2065b5fd1ed5f827758d1486fc3fbf24
hwangs12 <sungjun94@gmail.com>
2024-07-01 21:40:30 -0400

    destructuring

--- main.rs
+++ main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    // tuple 
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // destructuring
+    let (x, y, z) = tup;
+    println!("The value of y is: {y}");
 }
